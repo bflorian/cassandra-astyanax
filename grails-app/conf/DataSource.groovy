9ff53894805e3928c5fd033fe42fe94ef0cb024f
Bob Florian <bflorian@gmail.com>
2012-04-09 16:31:41 -0400

    Grails 2.0 upgrade.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+	pooled = true
+	driverClassName = "org.h2.Driver"
+	username = "sa"
+	password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -13,20 +13,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
+			url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+		}
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-        }
+			url = "jdbc:h2:mem:prodDb;MVCC=TRUE"
+		}
     }
 }
