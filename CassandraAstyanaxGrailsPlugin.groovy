cb929da840b6651daab6fd58b1d7795f0ae92472
Bob Florian <bflorian@gmail.com>
2012-08-23 15:41:41 -0400

    Version 0.1.0 (requesting publishing to Grails repo)

--- CassandraAstyanaxGrailsPlugin.groovy
+++ CassandraAstyanaxGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import com.reachlocal.grails.plugins.cassandra.astyanax.AstyanaxDynamicMethods
 class CassandraAstyanaxGrailsPlugin
 {
 	// the plugin version
-	def version = "0.0.13"
+	def version = "0.1.0"
 
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "2.0.0 > *"
