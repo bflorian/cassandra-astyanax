9637b4302b01953911c0fead9e91b7f4a47a24c1
Jeff Beck <jeff.beck@reachlocal.com>
2013-06-05 16:32:53 -0500

    Upgrade Grails and mark version as a snapshot

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,43 +3,42 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	// inherit Grails' default dependencies
+	inherits("global") {
+		// uncomment to disable ehcache
+		// excludes 'ehcache'
+	}
+	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		mavenCentral()
+	}
+	dependencies {
+		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
 		runtime 'com.github.stephenc.high-scale-lib:high-scale-lib:1.1.1'
 		runtime 'com.github.stephenc.eaio-uuid:uuid:3.2.0'
-		compile ('com.netflix.astyanax:astyanax:1.0.3') {
+		compile('com.netflix.astyanax:astyanax:1.0.3') {
 			excludes 'slf4j-log4j12', 'junit', 'commons-logging'
 		}
-    }
+
+		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+
+	}
 
 	plugins {
-		build (":svn:1.0.2") {
+
+		build(":tomcat:$grailsVersion",
+			":release:2.2.1",
+			":rest-client-builder:1.0.3") {
 			export = false
 		}
-		test (":spock:0.6") {
+		test(":spock:0.7") {
 			export = false
+			exclude "spock-grails-support"
 		}
+
 	}
 }
 
