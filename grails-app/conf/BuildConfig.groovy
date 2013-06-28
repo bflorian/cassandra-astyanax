a3e32633f3419bd31522ddcb9cac8f5600066d1c
Bob Florian <bflorian@gmail.com>
2013-06-28 13:39:32 -0400

    Upgraded Astyanax client to 1.56.42.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,9 +19,27 @@ grails.project.dependency.resolution = {
 
 		runtime 'com.github.stephenc.high-scale-lib:high-scale-lib:1.1.1'
 		runtime 'com.github.stephenc.eaio-uuid:uuid:3.2.0'
-		compile('com.netflix.astyanax:astyanax:1.0.3') {
+
+		compile('com.netflix.astyanax:astyanax-core:1.56.42') {
 			excludes 'slf4j-log4j12', 'junit', 'commons-logging'
 		}
+		compile('com.netflix.astyanax:astyanax-thrift:1.56.42') {
+			excludes 'slf4j-log4j12', 'junit', 'commons-logging'
+		}
+		compile('com.netflix.astyanax:astyanax-cassandra:1.56.42') {
+			excludes 'slf4j-log4j12', 'junit', 'commons-logging'
+		}
+
+		/*
+		| Error SLF4J: Class path contains multiple SLF4J bindings.
+		| Error SLF4J: Found binding in [jar:file:/Users/bflorian/.grails/ivy-cache/org.grails/grails-plugin-log4j/jars/grails-plugin-log4j-2.2.1.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+		| Error SLF4J: Found binding in [jar:file:/Users/bflorian/.grails/ivy-cache/org.slf4j/slf4j-log4j12/jars/slf4j-log4j12-1.7.2.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+		| Error SLF4J: See http://www.slf4j.org/codes.html#multiple_bindings for an explanation.
+		 */
+
+		//compile('com.netflix.astyanax:astyanax:1.0.3') {
+		//	excludes 'slf4j-log4j12', 'junit', 'commons-logging'
+		//}
 
 		test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
 
